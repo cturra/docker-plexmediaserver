d92a404175aab23ec15b878c190040cfc212fa65
cturra <cturra@gmail.com>
2015-12-21 14:29:31 -0800

    no longer using datestamp so lets make the comment more clear here.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ IMAGE_NAME="cturra/plex"
 BUILD_TAG=$(grep "ENV PLEX_SERVER_VERSION" Dockerfile| awk '{print $3}'| awk -F"." '{print $1"."$2"."$3"."$4}')
 DOCKER=$(which docker)
 
-# build image and tag with build tag (MonthDayYearMinute)
+# build image and tag with build tag (plex version number)
 $DOCKER build -t ${IMAGE_NAME}:${BUILD_TAG} .
 
 # add latest tag
